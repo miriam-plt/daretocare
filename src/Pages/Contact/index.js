f8e537b51d7ee462f3fcb9701f3baacd41383b52
Miriam Poletti <polettimiriam@gmail.com>
2022-11-21 16:21:49 +0100

    responsive design base

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -27,8 +27,8 @@ const Contact = () => {
         <Navbar />
         <div className='container'>
         <div className='contact-form'>
-            <h5 className='contact-me'>Get in touch</h5>
-            <p>If you have any inquiries about our work or would like to collaborate with us, please reach out to daretocare.artcollective@gmail.com or fill in the contact form below. We work on a part-time basis and will be in touch with you as soon as we can.</p>
+            <h5 className='contact-me contact-text'>Get in touch</h5>
+            <p className='contact-text'>If you have any inquiries about our work or would like to collaborate with us, please reach out to daretocare.artcollective@gmail.com or fill in the contact form below. We work on a part-time basis and will be in touch with you as soon as we can.</p>
             <form ref={form} onSubmit={sendEmail}>
                 <ul>
                     <li className='half'>
