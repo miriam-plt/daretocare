0c870adb89178becf0b0f29a3f1ec21194d52b9d
Miriam Poletti <polettimiriam@gmail.com>
2022-12-22 23:24:59 +0100

    class in contact form

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -31,7 +31,7 @@ const Contact = () => {
         <div className='container'>
         <div className='contact-form'>
             <h5 className='contact-me contact-text'>Get in touch</h5>
-            <p className='contact-text'>If you have any inquiries about our work or would like to collaborate with us, please reach out to daretocare.artcollective@gmail.com or fill in the contact form below.</p> <p> We work on a part-time basis and will be in touch with you as soon as we can.</p>
+            <p className='contact-text'>If you have any inquiries about our work or would like to collaborate with us, please reach out to daretocare.artcollective@gmail.com or fill in the contact form below.</p> <p className='contact-text'> We work on a part-time basis and will be in touch with you as soon as we can.</p>
             <form ref={form} onSubmit={sendEmail}>
                 <ul>
                     <li className='half'>
