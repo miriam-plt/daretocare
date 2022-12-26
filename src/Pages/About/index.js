f43fa64f5329de5e0f0c10589b23a4249bf0028e
Miriam Poletti <polettimiriam@gmail.com>
2022-12-26 23:56:38 +0100

    update manifesto and logo

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -20,19 +20,18 @@ const About = () => {
 
             <div className='manifesto'>
                 <p>We are a group of emerging artists from a wide range of different disciplines within the creative industry working across 
-                Europe. We have experience with mental health struggles caused by various aspects of our working environment and our 
-                professional experience. We decided to work together as <i>Dare to Care</i> to reflect on our experience, 
+                Europe. We have been dealing with mental health issues caused by various aspects of our working environment and 
+                professional experience. We decided to work together as <i>Dare to Care</i> to reflect on our situation, 
                 to share art which relates to mental health and to suggest structural improvements.</p>
-                <p>In our group, we have experiences of manipulation, body shaming, eating disorders, anxiety, exploitative emotional 
+                <p>In our group, we have experiences of manipulation, body shaming, eating disorders, anxiety, emotional 
                 labour, exploitation, financial precarity, humiliation, harassment, toxic competition, low self esteem and other issues 
                 and we have only been working for a few years. As individual art workers, we are often vulnerable and do not easily receive 
                 emotional support in our working environment. This is something we would like to change in our own careers and prevent 
                 from happening in careers of emerging and independent art workers no matter their age. </p>
-                <p>In our experience, mental health and artistic practice are intrinsically entangled: art can alleviate a bad mental 
-                health state, but it can also trigger negative emotions. Precisely because art provides the public opportunities for social gathering and 
-                open-minded communication, enabling them to build stronger and more cohesive communities, artists themselves need such opportunity. 
-                While through our work we contribute to the cultural wellbeing of communities, we also must look at the wellbeing of 
-                cultural workers. </p>
+                <p>In our experience, mental health and art practice are intrinsically entangled: art can alleviate a poor mental health state, 
+                but it can also trigger negative emotions. Since art provides opportunities for social gathering and open communication, 
+                enabling to build stronger and more cohesive communities, we believe that artists themselves need such opportunities. While 
+                through our work we contribute to the cultural well-being of others, we must also look at the well-being of cultural workers. </p>
                 <p>We believe that a conversation about mental health in the art world is long overdue, especially in relation to the 
                 working conditions of emerging and independent artists, and we invite you to join the conversation. Our name, <i>Dare to Care</i>, 
                 suggests that it is brave, useful and ultimately good to care about our surroundings and people, which we aim to do through 
