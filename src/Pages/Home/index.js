fe26ab263e0c80257d818598cec000b02c90c148
Miriam Poletti <polettimiriam@gmail.com>
2023-01-26 18:13:15 +0100

    visual identity update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 import { NavLink } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
-import image from '../../images/3d.png';
+
 
 const Home = () => {
     return (
@@ -9,14 +9,13 @@ const Home = () => {
         <Navbar />
         <div className='background-main'>
             <div className='header'>
-                <h1 className='title title-right'>Dare</h1>
-                <img className='image-between' src={image} alt='between' />
-                <h1 className='title title-left'>to Care</h1>
+                <h1 className='title title-top'>Dare</h1>  
+                <h1 className='title title-bottom'>to Care</h1>
+                <div className='section-1'>
+                    <h5 className='subtitle'>Art collective</h5>
+                    <NavLink to={'/about'}><button className='button-homepage'>Read our manifesto →</button></NavLink>
+                </div>
             </div>
-        </div>
-        <div className='section-1'>
-        <h5 className='subtitle'>Art collective</h5>
-        <NavLink to={'/about'}><button className='button-homepage'>Read our manifesto →</button></NavLink>
         </div>
     </div>
 )}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,14 +3,16 @@ import { Link, NavLink } from 'react-router-dom';
 import Logo from '../../images/logo19.png';
 
 const Navbar = () => {
-    return <div className='nav-bar'>
-        <div className='nav-logo'>
-            <Link className='logo-link' to='/'><img className='logo' src={Logo} alt='logo' /></Link>
+    return <div className='nav-container'>
+        <div className='nav-bar'>
+            <div className='nav-logo'>
+                <Link className='logo-link' to='/'><img className='logo' src={Logo} alt='logo' /></Link>
+            </div>
+            <ul className='nav-links'>
+                <li><NavLink className={({ isActive }) => isActive ? 'nav-link selected' : 'nav-link'} to='/about'>About</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? 'nav-link selected' : 'nav-link'} to='/contact'>Contact</NavLink></li>
+            </ul>
         </div>
-        <ul className='nav-right'>
-            <li><NavLink className={({ isActive }) => isActive ? 'nav-link selected' : 'nav-link'} to='/about'>About</NavLink></li>
-            <li><NavLink className={({ isActive }) => isActive ? 'nav-link selected' : 'nav-link'} to='/contact'>Contact</NavLink></li>
-        </ul>
     </div>
 }
 
